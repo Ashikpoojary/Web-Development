6bc43c47a2c9d873dece98779e9db75627a4c9ba
Ashikpoojary <ashikpoojary06@gmail.com>
2024-02-08 21:29:07 +0530

    Added Features

--- Dinosaur Game/script.js
+++ Dinosaur Game/script.js
@@ -34,6 +34,14 @@ let cactus1Img;
 let cactus2Img;
 let cactus3Img;
 
+//physics
+let velocityX = -8; //cactus moving left speed
+let velocityY = 0;
+let gravity = .4;
+
+let gameOver = false;
+let score = 0;
+
 window.onload= function(){
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -57,13 +65,55 @@ window.onload= function(){
 
     requestAnimationFrame(update);
     setInterval(placeCactus,1000);
+    document.addEventListener("keydown",moveDino);
     }
 }
 
 // After every jump dino is going to be drawn again to Y axis
 function update(){ 
     requestAnimationFrame(update);
+    if(gameOver){
+        return;
+    }
+
+    context.clearRect(0,0,board.width,board.height);
+
+    //dino
+    velocityY += gravity;
+    dino.y = Math.min(dino.y + velocityY, dinoY); //apply gravity to current dino.y, making sure it doesn't exceed the ground
     context.drawImage(dinoImg, dino.x,dino.y,dino.width,dino.height);
+    //cactus
+    for(let i=0;i<cactusArray.length; i++){
+        let cactus = cactusArray[i];
+        cactus.x += velocityX; 
+        context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+
+        if(detectCollision(dino,cactus)){
+            gameOver = true;
+            dinoImg.src = "./img/dino-dead.png";
+            dinoImg.onload = function(){
+                context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+            }
+        }
+    }
+
+    //score   
+    context.fillStyle="black";
+    context.font="20px courier";
+    score++;
+    context.fillText(score,5,20);
+
+}
+
+function moveDino(e){
+    if(gameOver){
+        return;
+    }
+
+    if((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY){
+        //jump
+        velocityY = -10;
+    }
 }
 
 function placeCactus(){
@@ -74,4 +124,33 @@ function placeCactus(){
         width : null,
         height : cactusHeight
     }
+
+    let placeCactusChance = Math.random();
+
+    if(placeCactusChance > .90){
+        cactus.img = cactus3Img;
+        cactus.width = cactus3Width;
+        cactusArray.push(cactus);
+    } 
+    else if(placeCactusChance > .70){ //30% you get cactus2
+        cactus.img = cactus2Img;
+        cactus.width = cactus2Width;
+        cactusArray.push(cactus);
+    }
+    else if(placeCactusChance > .50){
+        cactus.img = cactus1Img;
+        cactus.width = cactus1Width;
+        cactusArray.push(cactus);
+    }
+
+    if(cactusArray.length > 5){
+        cactusArray.shift(); //remove first element from array , so that arrray doesn't constantly grow;
+    }
+}
+
+function detectCollision(a,b){
+    return a.x < b.x + b.width && // a's top left corner doesn't reach b's top right corner
+           a.x + a.width > b.x && // a's top right corner passes b's top left corner
+           a.y < b.y + b.height && // a's top left corner doesn't reach b's bottom left corner
+           a.y + a.height > b.y; // a's bottom left corner passes b's top left corner
 }
