5945a8dc57f13d213fb911a90fb16ae28b3687b6
Ashikpoojary <ashikpoojary06@gmail.com>
2024-02-13 20:02:00 +0530

    Added Details

--- PokeDex/script.js
+++ PokeDex/script.js
@@ -53,7 +53,7 @@ function displayPokemons(pokemon) {
     listItem.addEventListener("click", async () => {
       const success = await fetchPokemonDataBeforeRedirect(pokemonID);
       if (success) {
-        window.location.href = `./detail.html?id=${pokemonID}`;
+        window.location.href = `./index-detail.html?id=${pokemonID}`;
       }
     });
 
